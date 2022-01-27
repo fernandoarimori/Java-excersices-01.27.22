b1594091d02afe890b7158ecb1e73bd1c2df053d
Fernando Castilho Arimori <fernando.c.arimori@gmail.com>
2022-01-27 14:11:25 -0300

    loop java exercises

---
+++ Laço repetição/src/Exercise6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inp= new Scanner(System.in);
+		int ent, som, med, cont;
+		ent=0;
+		som=0;
+		cont=0;
+		do
+		{
+			System.out.println("Digite um número: ");
+			ent=inp.nextInt();
+			som=som+ent;
+			cont++;
+				
+		}while (ent!=0);
+		med=som/(cont-1);
+		System.out.println("A média dos números entrados é de:"+ med+".");
+		inp.close();
+	}
+
+}
---
+++ Laço repetição/src/Exerxise5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exerxise5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in= new Scanner(System.in);
+		int x, y;
+		x=0;
+		y=0;
+		do
+		{
+			System.out.print("Digite um número: \n");
+			y= in.nextInt();
+			x=y+x;
+			
+		}while (y!=0);
+		System.out.print("A soma total dos número é: "+x);	
+		in.close();
+	}
+
+}
